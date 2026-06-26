802f56343c4be38a8a641bb1760c0cfee5309c26
aceydufresne <166555240+aceydufresne@users.noreply.github.com>
2026-06-26 14:41:38 -0400

    Add uploadRes function to handle various file types

---
+++ methods/mainNode.py
@@ -0,0 +1,62 @@
+import fitz
+import os
+import pandas as pd
+from locationEmbedding import findLoc
+import spacy
+from pypdf import PdfReader
+from docx import Document
+
+def uploadRes(resPath):
+    
+    #sort based upon if the input is already a txt format,
+    #or pdf extension
+    extVar = None
+    extension = resPath.split(".")
+    if extension[-1] == "pdf":
+        reader = PdfReader(resPath)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+        lines = text.split("\n")
+        lines = [line.strip() for line in lines if line.strip()]
+        return extVar, lines
+    elif extension[-1] == "docx":
+        doc = Document(resPath)
+        lines = []
+        for line in doc.paragraphs:
+            lines.append(line.text)
+        return extVar, lines
+    elif extension[-1] == "txt":
+        #input is already a string
+        with open(resPath, "r", encoding="utf-8") as file:
+            lines = [line.strip() for line in file]
+        return extVar, lines
+    else:
+        negResponse = "Error in extension type"
+        return extVar, 
+
+if __name__ == "__main__":
+    model = spacy.load("en_core_web_sm")
+    #input is an environmental variable
+    citiesCSV = "C:\\Users\\Acey\\Downloads\\Dove Agent Build\\Datasets\\uscities.csv"
+    cities = pd.read_csv(citiesCSV)
+    inputPath =  "C:\\Users\\Acey\\Downloads\\Dove Agent Build\\Agent Modules\\Dufresne Resume Spring 2025 (1).pdf"
+    
+    resExam = "C:\\Users\\Acey\\Downloads\\Dove Agent Build\\Datasets\\Resume.csv"
+    resumes = pd.read_csv(resExam)
+    example = resumes["Resume_str"]
+    
+    cityPop = "C:\\Users\\Acey\\Downloads\\Dove Agent Build\\Datasets\\cityPop.csv"
+    cityPopulation = pd.read_csv(cityPop)
+    
+    extVar, text = uploadRes(inputPath)
+    
+    findLoc(text,cities, cityPopulation)
+    
+    for i in range(20):
+        lines = example[i].split("\n")
+        
+        city, state = findLoc(lines, cities, cityPopulation)
+        print(f"Resume #{i}\nCity: {city}\nState: {state}")
+
+        #print(f"Resume #{i}\n\n {lines}")
