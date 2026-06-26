442c05efcc02fcf20a6ea2ff351364879996a951
aceydufresne <166555240+aceydufresne@users.noreply.github.com>
2026-06-26 16:55:07 -0400

    Implement TF-IDF functions and modify findLoc
    Added TF-IDF calculation functions and updated findLoc to accept tfScores.

--- methods/locationPrediction.py
+++ methods/locationPrediction.py
@@ -1,8 +1,10 @@
 import spacy
 import csv
 import pandas as pd
+import string
+import math
 
-def findLoc(input1, cities, cityPop):
+def findLoc(input1, cities, cityPop, tfScores):
     stateNames = set(cities["state_name"].astype(str).str.upper())
     stateIDs = set(cities["state_id"].astype(str).str.upper())
     cityNames = set(cities["city"].astype(str).str.upper())
@@ -92,3 +94,84 @@ def findLoc(input1, cities, cityPop):
             finalCityPrediction = city
             break
     return finalCityPrediction, finalStatePrediction
+
+def tfIDF(inputTerm, resumes):
+    inputTerm = inputTerm.strip(string.punctuation).lower()
+    allRes = []
+
+    for resume in resumes:
+        tf = {}
+        finalTF = {}
+        lenTF = 0
+        lines = resume.split("\n")
+
+        for line in lines:
+            terms = line.split()
+            for term in terms:
+                term = term.strip(string.punctuation).lower()
+                if term == "":
+                    continue
+                tf[term] = tf.get(term, 0) + 1
+                lenTF += 1
+        for term in tf:
+            finalTF[term] = tf[term] / lenTF
+
+        allRes.append(finalTF)
+    N = len(resumes)
+    DF = 0
+    for resumeTF in allRes:
+        if inputTerm in resumeTF:
+            DF += 1
+    if DF == 0:
+        return 0
+    idf = math.log(N / DF)
+    scores = []
+    for resumeTF in allRes:
+        tfScore = resumeTF.get(inputTerm, 0)
+        tfidfScore = tfScore * idf
+        scores.append(tfidfScore)
+
+    return scores
+
+
+def findTF(resumes):
+    allTF = []
+
+    for resume in resumes:
+        tf = {}
+        totalTerms = 0
+        for term in resume.split():
+            term = term.strip(string.punctuation).lower()
+
+            if term == "":
+                continue
+            tf[term] = tf.get(term, 0) + 1
+            totalTerms += 1
+        finalTF = {}
+
+        for term in tf:
+            finalTF[term] = tf[term] / totalTerms
+        allTF.append(finalTF)
+
+    return allTF
+
+def findTfIDF(inputTerm, allTF):
+    
+    inputTerm = inputTerm.strip(string.punctuation).lower()
+    N = len(allTF)
+    DF = 0
+    
+    for resumeTF in allTF:
+        if inputTerm in resumeTF:
+            DF += 1
+    if DF == 0:
+        return [0] * N
+    
+    idf = math.log(N / DF)
+    scores = []
+
+    for resumeTF in allTF:
+        tfScore = resumeTF.get(inputTerm, 0)
+        scores.append(tfScore * idf)
+
+    return scores
